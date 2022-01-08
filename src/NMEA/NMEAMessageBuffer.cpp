b0bbbae776e2924853adc560e3e81a6c67f8461f
Lisa Rowell <lisa.a.rowell@me.com>
2022-01-08 15:23:59 -0800

    Fixed the forgotten buffer size check in the previous commit.

--- src/NMEA/NMEAMessageBuffer.cpp
+++ src/NMEA/NMEAMessageBuffer.cpp
@@ -9,13 +9,16 @@
 #include "NMEAGSVMessage.h"
 #include "NMEARMCMessage.h"
 #include "NMEATXTMessage.h"
+#include "NMEAVDMMessage.h"
 #include "NMEAVTGMessage.h"
 
 #define MAX(a,b) (((a)>(b))?(a):(b))
 
-#define NMEA_MESSAGE_BUFFER_SIZE (MAX(MAX(MAX(sizeof(NMEAGGAMessage), sizeof(NMEAGSAMessage)), \
-                                          MAX(sizeof(NMEAGLLMessage), sizeof(NMEAGSTMessage))), \
-                                      MAX(MAX(sizeof(NMEAGSVMessage), sizeof(NMEARMCMessage)), \
-                                          MAX(sizeof(NMEATXTMessage), sizeof(NMEAVTGMessage)))))
+#define NMEA_MESSAGE_BUFFER_SIZE \
+(MAX(MAX(MAX(MAX(sizeof(NMEAGGAMessage), sizeof(NMEAGSAMessage)), \
+             MAX(sizeof(NMEAGLLMessage), sizeof(NMEAGSTMessage))), \
+         MAX(MAX(sizeof(NMEAGSVMessage), sizeof(NMEARMCMessage)), \
+             MAX(sizeof(NMEATXTMessage), sizeof(NMEAVDMMessage)))), \
+     sizeof(NMEAVTGMessage)))
 
 uint8_t nmeaMessageBuffer[NMEA_MESSAGE_BUFFER_SIZE];
