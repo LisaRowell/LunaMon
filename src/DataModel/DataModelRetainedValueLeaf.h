856b076a5d5635841c18d1feab58e82099cbfdf4
Lisa Rowell <lisa.a.rowell@me.com>
2022-11-01 22:41:32 -0700

    Fixed out of style formatting.

--- src/DataModel/DataModelRetainedValueLeaf.h
+++ src/DataModel/DataModelRetainedValueLeaf.h
@@ -6,15 +6,15 @@
 #include <stdint.h>
 
 class DataModelRetainedValueLeaf : public DataModelLeaf {
-   private:
-    bool hasBeenSet;
+    private:
+        bool hasBeenSet;
 
-   protected:
-    DataModelRetainedValueLeaf(const char *name, DataModelElement *parent);
-    virtual bool subscribe(DataModelSubscriber &subscriber, uint32_t cookie) override;
-    void updated();
-    bool hasValue() const;
-    virtual void sendRetainedValue(DataModelSubscriber &subscriber) = 0;
+    protected:
+        DataModelRetainedValueLeaf(const char *name, DataModelElement *parent);
+        virtual bool subscribe(DataModelSubscriber &subscriber, uint32_t cookie) override;
+        void updated();
+        bool hasValue() const;
+        virtual void sendRetainedValue(DataModelSubscriber &subscriber) = 0;
 };
 
 #endif
